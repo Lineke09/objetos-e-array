da2621a1e2d67f092f95cfacdff51a476775f949
Lineke09 <109102261+Lineke09@users.noreply.github.com>
2022-09-15 18:32:14 -0300

    Create objetosarray.js

---
+++ objetosarray.js
@@ -0,0 +1,17 @@
+
+
+var pessoas = ['jorge', 'carlos', 'pedro']
+let pessoa = {
+    nome: 'Jorge',
+    idade: 20,
+    altura: 1.80,
+
+
+
+    listar: function () {
+        return nome, idade, altura;
+    },
+
+    listararray: function () {
+        return pessoas;
+} };
